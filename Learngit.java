8fec8bacb6c07e9bbc87e6320050c79cf7d4d6fa
Rajani Roy <roy282554@gmail.com>
2024-08-15 21:17:30 +0530

    1st commit

---
+++ Learngit.java
@@ -0,0 +1,13 @@
+package gitLearn;
+
+public class Learngit {
+
+    public static void main(String[] args) {
+
+        int a = 34;
+        int b = 20;
+
+        int result = a + b;
+        System.out.println(result);
+    }
+}
---
+++ seconndFile.java
@@ -0,0 +1,10 @@
+public class seconndFile {
+    public static void main(String[] args) {
+
+        int a = 94;
+        int b = 20;
+
+        int result = a + b;
+        System.out.println(result);
+    }
+}
